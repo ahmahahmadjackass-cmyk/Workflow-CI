649a5198e3581783273b0329988a0d27ec780656
unknown <ahmahahmadjackass@gmail.com>
2025-10-08 16:47:08 +0700

    first

---
+++ MLProject/modelling.py
@@ -0,0 +1,21 @@
+import mlflow
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+mlflow.autolog()
+
+data = pd.read_csv('./winequality_white_preprocessing.csv') 
+
+X = data.drop('quality_encoded', axis=1)
+y = data['quality_encoded']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+with mlflow.start_run(run_name="Basic_RandomForest_Autolog"):
+
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+    print(f"Accuracy: {accuracy}")
